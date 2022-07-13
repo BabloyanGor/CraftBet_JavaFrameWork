e0634c9b95cae76774350c8b6666ad9b856c4df1
Gor Babloyan <g.babloyan@iqsoft.am>
2022-07-13 12:06:29 +0400

    test

--- craftBetAutomation/run.bat
+++ craftBetAutomation/run.bat
@@ -1,4 +1,5 @@
 cd C:\Users\Nerses Khachatryan\Desktop\Git_craftBet_TestAutomation\CraftBet_JavaFrameWork\craftBetAutomation
 mvn clean install
 cd C:\Users\Nerses Khachatryan\Desktop\Git_craftBet_TestAutomation\CraftBet_JavaFrameWork\craftBetAutomation
-allure serve allure-results
+allure serve .\allure-results\
+
